656bc77ccee1a8a47659a761bd5e1f60358721c6
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-16 00:40:20 -0500

    completed tests

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -147,7 +147,18 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
+  let newArr = [];
+
+  arr.forEach(ele => {
+    if (ele % 15 === 0){
+      newArr.push("Fizz Buzz");
+    } else if (ele % 5 === 0) {
+      newArr.push("Buzz");
+    } else if (ele % 3 === 0) {
+      newArr.push("Fizz");
+    } else newArr.push(ele)
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -201,7 +212,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
